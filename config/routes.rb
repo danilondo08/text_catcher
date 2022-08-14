27a9a5f4cdbc3af2185e57803fef649d84dd9b85
Daniela Londoño <londonodaniela0653@gmail.com>
2022-08-14 08:30:03 -0500

    Fixes devise sign out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
   # root 'home#index'
-  devise_for :users
+  devise_for :users, sign_out_via: [:get] 
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'texts_catcher#new' 
   resources :texts_catcher, only: [:show, :create, :new, :index]
+  resources :all_user_images, only: [:index]
 end
